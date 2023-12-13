318367d043b52ddbb2900ee008a04734196da112
VishnuNampoothiriG <146819074+VishnuNampoothiriG@users.noreply.github.com>
2023-12-13 09:52:31 +0530

    Add files via upload

---
+++ co4/third.py
@@ -0,0 +1,16 @@
+class rectangle:
+    def __init__(self,length,breadth):
+        self.__length=length
+        self.__breadth=breadth
+    def area(self):
+        return self.__length*self.__breadth
+    def __it__(self,other):
+        return self.area<other.area
+obj1=rectangle(6,4)
+obj2=rectangle(4,5)
+area1=obj1.area()
+area2=obj2.area()
+if(area1<area2):
+    print(area2,"(area2) is larger")
+else:
+    print(area1,"(area1) is larger")
